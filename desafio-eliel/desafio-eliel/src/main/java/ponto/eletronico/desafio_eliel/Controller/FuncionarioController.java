f0bb7f09fe2c33545a73bdde1685a96378f7148c
Diogo <dio_valgas@outlook.com>
2025-11-06 19:37:20 -0300

    Adicionado PUT E DELETE

--- desafio-eliel/desafio-eliel/src/main/java/ponto/eletronico/desafio_eliel/Controller/FuncionarioController.java
+++ desafio-eliel/desafio-eliel/src/main/java/ponto/eletronico/desafio_eliel/Controller/FuncionarioController.java
@@ -1,6 +1,7 @@
 package ponto.eletronico.desafio_eliel.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import ponto.eletronico.desafio_eliel.Model.FuncionarioModel;
 import ponto.eletronico.desafio_eliel.Service.FuncionarioService;
@@ -24,6 +25,24 @@ public class FuncionarioController {
     @PostMapping
     public FuncionarioModel criarFuncionario(@RequestBody FuncionarioModel funcionario) {
         return funcionarioService.criarFuncionario(funcionario);
+    }
+
+    @PutMapping("/{id}")
+    public FuncionarioModel atualizarFuncionario(
+            @PathVariable Long id,
+            @RequestBody FuncionarioModel dados) {
+        return funcionarioService.atualizarFuncionario(id, dados);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deletarFuncionario(@PathVariable Long id) {
+        funcionarioService.deletarFuncionario(id);
+    }
+
+    @GetMapping("/bloqueados")
+    public List<FuncionarioModel> listarBloqueados() {
+        return funcionarioService.listarBloqueados();
     }
 
 
--- desafio-eliel/desafio-eliel/src/main/java/ponto/eletronico/desafio_eliel/Service/FuncionarioService.java
+++ desafio-eliel/desafio-eliel/src/main/java/ponto/eletronico/desafio_eliel/Service/FuncionarioService.java
@@ -1,17 +1,22 @@
 package ponto.eletronico.desafio_eliel.Service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 import ponto.eletronico.desafio_eliel.Model.FuncionarioModel;
 import ponto.eletronico.desafio_eliel.repository.FuncionarioRepository;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class FuncionarioService {
 
+
     @Autowired
     private FuncionarioRepository funcionarioRepository;
+
 
     public List<FuncionarioModel> listarFuncionarios() {
         return funcionarioRepository.findAll();
@@ -20,4 +25,17 @@ public class FuncionarioService {
     public FuncionarioModel criarFuncionario (FuncionarioModel funcionario) {
         return funcionarioRepository.save(funcionario);
     }
+
+    public void deletarFuncionario(Long id) {
+        funcionarioRepository.deleteById(id);
+    }
+
+    public FuncionarioModel atualizarFuncionario(Long id, FuncionarioModel funcionario) {
+        FuncionarioModel novoFuncionario = funcionarioRepository.findById(id).get();
+        novoFuncionario.setHoraEntrada(funcionario.getHoraEntrada());
+        novoFuncionario.setMinutosEntrada(novoFuncionario.getMinutosEntrada());
+        novoFuncionario.setSegundosEntrada(novoFuncionario.getSegundosEntrada());
+        return funcionarioRepository.save(novoFuncionario);
+    }
+
 }
